8526e2b9e4a41a5a170063fee9ec201aa96d2407
Waldemar Kornewald <wkornewald>
2010-12-25 12:01:08 +0100

    improved autodetect() based on admin.autodetect() (though, without reset of registry)

--- dbindexer/__init__.py
+++ dbindexer/__init__.py
@@ -3,44 +3,25 @@ def autodiscover():
     Automatically loads database index definitions from db_indexes modules in
     installed apps.
     """
-    import imp
     from django.conf import settings
     from django.utils.importlib import import_module
+    from django.utils.module_loading import module_has_submodule
 
     for app in settings.INSTALLED_APPS:
-        # For each app, we need to look for an search_indexes.py inside that app's
-        # package. We can't use os.path here -- recall that modules may be
-        # imported different ways (think zip files) -- so we need to get
-        # the app's __path__ and look for search_indexes.py on that path.
-
-        # Step 1: find out the app's __path__ Import errors here will (and
-        # should) bubble up, but a missing __path__ (which is legal, but weird)
-        # fails silently -- apps that do weird things with __path__ might
-        # need to roll their own index registration.
+        mod = import_module(app)
+        # Attempt to import the app's admin module.
         try:
-            app_path = import_module(app).__path__
-        except AttributeError:
-            continue
-
-        # Step 2: use imp.find_module to find the app's search_indexes.py. For some
-        # reason imp.find_module raises ImportError if the app can't be found
-        # but doesn't actually try to import the module. So skip this app if
-        # its search_indexes.py doesn't exist
-        try:
-            imp.find_module('dbindexes', app_path)
-        except ImportError:
-            continue
-
-        # Step 3: import the app's search_index file. If this has errors we want them
-        # to bubble up.
-        import_module("%s.dbindexes" % app)
+            import_module('%s.dbindexes' % app)
+        except:
+            # Decide whether to bubble up this error. If the app just
+            # doesn't have an admin module, we can ignore the error
+            # attempting to import it, otherwise we want it to bubble up.
+            if module_has_submodule(mod, 'dbindexes'):
+                raise
 
 def load_indexes():
     from django.conf import settings
     from django.utils.importlib import import_module
 
     for name in getattr(settings, 'DB_INDEX_MODULES', ()):
-        try:
-            import_module(name)
-        except ImportError:
-            pass
+        import_module(name)
