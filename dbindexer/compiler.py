f1ea60a8f9b3ac25f0c4236c2c01f933d3c0d858
Thomas Wanschik <twanschik>
2011-01-29 11:46:31 +0100

    typo

--- dbindexer/compiler.py
+++ dbindexer/compiler.py
@@ -9,7 +9,7 @@ from django.db.models.sql.where import Constraint
 Constraint.__repr__ = __repr__
 
 # TODO: manipulate a copy of the query instead of the query itself. This has to
-# be done because the query can be reused afterwoods by the user so that a
+# be done because the query can be reused afterwards by the user so that a
 # manipulated query can result in strange behavior for these cases!
 #TODO: Add watching layer which gives suggestions for indexes via query inspection
 # at runtime
