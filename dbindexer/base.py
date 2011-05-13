579822f910d2f1319b9dee2b91ce12fc248a4f49
Waldemar Kornewald <wkornewald>
2011-05-13 10:57:48 +0200

    reimplemented database backend proxying via inheritance instead of __getattr__

--- dbindexer/base.py
+++ dbindexer/base.py
@@ -1,32 +1,34 @@
-from django.db import connections
+from django.conf import settings
 from django.utils.importlib import import_module
 
-class Proxy(object):
-    def __init__(self, target):
-        self._target = target
+class DatabaseOperations(object):
+    dbindexer_compiler_module = __name__.rsplit('.', 1)[0] + '.compiler'
 
-    def __getattr__(self, name):
-        return getattr(self._target, name)
-
-class OperationsProxy(Proxy):
-    compiler_module = __name__.rsplit('.', 1)[0] + '.compiler'
-
-    def __init__(self, *args, **kwargs):
-        super(OperationsProxy, self).__init__(*args, **kwargs)
-        self._cache = {}
+    def __init__(self):
+        self._dbindexer_cache = {}
 
     def compiler(self, compiler_name):
-        if compiler_name not in self._cache:
-            target = self._target.compiler(compiler_name)
+        if compiler_name not in self._dbindexer_cache:
+            target = super(DatabaseOperations, self).compiler(compiler_name)
             base = getattr(
-                import_module(self.compiler_module), compiler_name)
+                import_module(self.dbindexer_compiler_module), compiler_name)
             class Compiler(base, target):
                 pass
-            self._cache[compiler_name] = Compiler
-        return self._cache[compiler_name]
+            self._dbindexer_cache[compiler_name] = Compiler
+        return self._dbindexer_cache[compiler_name]
 
-class DatabaseWrapper(Proxy):
-    def __init__(self, settings_dict, *args, **kwds):
-        super(DatabaseWrapper, self).__init__(
-            connections[settings_dict['TARGET']])
-        self.ops = OperationsProxy(self.ops)
+class BaseDatabaseWrapper(object):
+    def __init__(self, *args, **kwargs):
+        super(BaseDatabaseWrapper, self).__init__(*args, **kwargs)
+        class Operations(DatabaseOperations, self.ops.__class__):
+            pass
+        self.ops.__class__ = Operations
+        self.ops.__init__()
+
+def DatabaseWrapper(settings_dict, *args, **kwargs):
+    target_settings = settings.DATABASES[settings_dict['TARGET']]
+    engine = target_settings['ENGINE'] + '.base'
+    target = import_module(engine).DatabaseWrapper
+    class Wrapper(BaseDatabaseWrapper, target):
+        pass
+    return Wrapper(settings_dict, *args, **kwargs)
