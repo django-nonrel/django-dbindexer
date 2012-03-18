30161167b922782a4ee29b106eba856470f8a74c
Alex Burgel <aburgel@gmail.com>
2012-03-18 19:45:41 -0400

    spacing cleanup

--- dbindexer/compiler.py
+++ dbindexer/compiler.py
@@ -11,7 +11,7 @@ Constraint.__repr__ = __repr__
 # TODO: manipulate a copy of the query instead of the query itself. This has to
 # be done because the query can be reused afterwards by the user so that a
 # manipulated query can result in strange behavior for these cases!
-#TODO: Add watching layer which gives suggestions for indexes via query inspection
+# TODO: Add watching layer which gives suggestions for indexes via query inspection
 # at runtime
 
 class BaseCompiler(object):
