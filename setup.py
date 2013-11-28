82f39ceaeaa709ae0bfc058164258f41499e5c65
Alex Burgel <aburgel@gmail.com>
2013-11-28 12:56:18 -0500

    Change setup.py to use distutils

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+import os
 
-DESCRIPTION = 'Expressive NoSQL'
+def get_packages(package):
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+DESCRIPTION = 'Expressive NoSQL for Django'
 LONG_DESCRIPTION = None
 try:
     LONG_DESCRIPTION = open('README.rst').read()
@@ -8,23 +15,26 @@ except:
     pass
 
 setup(name='django-dbindexer',
-      version='0.3',
-      packages=find_packages(),
-      author='Waldemar Kornewald, Thomas Wanschik',
-      author_email='team@allbuttonspressed.com',
-      url='http://www.allbuttonspressed.com/projects/django-dbindexer',
+      version='1.6.0',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
-      platforms=['any'],
+      author='Waldemar Kornewald, Thomas Wanschik',
+      author_email='team@allbuttonspressed.com',
+      url='https://github.com/django-nonrel/django-dbindexer',
+      packages=get_packages('dbindexer'),
+      license='3-clause BSD',
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
           'Framework :: Django',
           'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
           'Topic :: Software Development :: Libraries :: Application Frameworks',
           'Topic :: Software Development :: Libraries :: Python Modules',
-          'License :: OSI Approved :: BSD License',
       ],
 )
